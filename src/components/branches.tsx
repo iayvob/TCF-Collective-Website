272336c9dcf3ac3d977124e8ee6db92217eaf3e2
iayvob <iayvob@gmail.com>
2024-12-18 19:11:40 +0100

    update

--- src/components/branches.tsx
+++ src/components/branches.tsx
@@ -2,7 +2,31 @@ import React from "react";
 import { motion } from "framer-motion";
 import { fadeInAnimationVariantsDiv } from "../lib/animations";
 import { ThreeDCardExample } from "./snippets/3d-card-snippet";
-import grap from "../../public/images/Events/youtube.png";
+import img from "../../public/images/companies/sss.png";
+
+const companies = [
+  {
+    header: "The Content School",
+    description:
+      "A platform dedicated to equipping by tools needed to master the art of content creation.",
+    img: img,
+    link: "https://www.youtube.com/@aburob",
+  },
+  {
+    header: "TCF Service",
+    description:
+      "Your trusted partner in delivering tailored services that meet the unique needs of businesses and individuals.",
+    img: img,
+    link: "https://www.youtube.com/@aburob",
+  },
+  {
+    header: "Arabfluence",
+    description:
+      "Hub Drive impactful collaborations and authentic storytelling.",
+    img: img,
+    link: "https://www.youtube.com/@aburob",
+  },
+];
 
 export default function Branches() {
   return (
@@ -15,30 +39,15 @@ export default function Branches() {
       }}
     >
       <div className="flex flex-col md:flex-row justify-between gap-x-4">
-        <ThreeDCardExample
-          header={"The Content School"}
-          description={
-            "A platform dedicated to equipping by tools needed to master the art of content creation."
-          }
-          img={grap}
-          link={"https://www.youtube.com/@aburob"}
-        />
-        <ThreeDCardExample
-          header={"TCF Service"}
-          description={
-            "Your trusted partner in delivering tailored services that meet the unique needs of businesses and individuals."
-          }
-          img={grap}
-          link={"https://www.youtube.com/@aburob"}
-        />
-        <ThreeDCardExample
-          header={"Arabfluence"}
-          description={
-            "Hub Drive impactful collaborations and authentic storytelling."
-          }
-          img={grap}
-          link={"https://www.youtube.com/@aburob"}
-        />
+        {companies.map((companie, index) => (
+          <ThreeDCardExample
+            key={index}
+            header={companie.header}
+            description={companie.description}
+            img={companie.img}
+            link={companie.link}
+          />
+        ))}
       </div>
     </motion.div>
   );
